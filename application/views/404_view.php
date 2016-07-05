ca169b3ef95c97088de9ee3bf1e0425296aa056d
Baloo-Mova <sergious-91@mail.ru>
2016-07-05 14:22:51 +0300

    Создан каркас MVC приложения; Добавлена 404 страница;

---
+++ application/views/404_view.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<!-- saved from url=(0044)https://ukieweb.com/envato/404-error/monkey/ -->
+<html lang="en" class=" js flexbox flexboxlegacy canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers applicationcache svg inlinesvg smil svgclippaths" style="overflow-y: hidden;"><!--<![endif]--><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <title>Monkey</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <!-- Libs CSS -->
+    <link type="text/css" media="all" href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Template CSS -->
+    <link type="text/css" media="all" href="css/404style.css" rel="stylesheet">
+    <!-- Responsive CSS -->
+    <link type="text/css" media="all" href="css/respons.css" rel="stylesheet">  
+</head>
+<body>
+
+    <!-- Load page -->
+    <div class="animationload" style="display: none;">
+        <div class="loader" style="display: none;">
+        </div>
+    </div>
+    <!-- End load page -->
+
+
+    <!-- Content Wrapper -->
+    <div id="wrapper">
+        <div class="container">
+            <div class="col-xs-12 col-sm-7 col-lg-7">
+                <!-- Info -->
+                <div class="info">
+                    <h1>404</h1>
+                    <h2>страница не найдена</h2>
+                    <p>страница которую вы ищите возможно была переименована, перемещена, удалена или вообще не существовала. </p>
+                    <a href="/test_bwt/" class="btn">Домой</a>
+                    <a href="/test_bwt/feedback" class="btn btn-brown">Напишите нам</a>
+                </div>
+                <!-- end Info -->
+            </div>
+
+            <div class="col-xs-12 col-sm-5 col-lg-5 text-center">
+                <!-- Monkey -->
+                <div class="monkey">
+                    <img src="images/monkey.gif" alt="Monkey">
+                </div>
+                <!-- end Monkey -->
+            </div>
+
+        </div>
+        <!-- end container -->
+    </div>
+    <!-- end Content Wrapper -->
+
+
+    <!-- Scripts -->
+    <script src="./Monkey_files/jquery-2.1.1.min.js" type="text/javascript"></script>
+    <script src="./Monkey_files/bootstrap.min.js" type="text/javascript"></script>
+    <script src="./Monkey_files/modernizr.custom.js" type="text/javascript"></script>
+    <script src="./Monkey_files/jquery.nicescroll.min.js" type="text/javascript"></script>
+    <script src="./Monkey_files/scripts.js" type="text/javascript"></script><div id="ascrail2000" class="nicescroll-rails" style="width: 4px; z-index: 999999; cursor: default; position: fixed; top: 0px; height: 100%; right: 0px; display: none;"><div style="position: relative; top: 0px; float: right; width: 2px; height: 0px; border: 1px solid rgb(255, 255, 255); border-radius: 5px; background-color: rgb(255, 255, 255); background-clip: padding-box;"></div></div>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+
+</body></html>
---
+++ application/views/template_view.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+        <title>Тестовое задание</title>
+        <link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css" />
+        <link href="http://fonts.googleapis.com/css?family=Kreon" rel="stylesheet" type="text/css" />
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" type="text/css" href="css/style.css"  />
+    </head>
+    <body>
+        
+        <div class="container">
+            <div class="row">
+                <div class="navbar navbar-default" role="navigation">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    </div>
+                    
+                    <div class="navbar-collapse collapse">
+                      <ul class="nav navbar-nav menu">
+                        <li><a href="/test_bwt/">Главная</a></li>
+                        <li><a href="/test_bwt/user/registraion">Регистрация</a></li>
+                        <li><a href="/test_bwt/weather">Погода</a></li>
+                        <li><a href="/test_bwt/feedback">Форма обратной связи</a></li>
+                        <li><a href="/test_bwt/feedbacklist">Список</a></li>
+                        <li class="navbar-nav navbar-right"><a href="/test_bwt/user/login">Вход</a></li>
+                      </ul>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+                
+        
+        <div class="container">
+             <?php include 'application/views/' . $content_view; ?> 
+        </div>
+
+        <script src="js/jquery-1.9.1.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    </body>
+</html>
